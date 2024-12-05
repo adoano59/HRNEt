ff10f21c209ec6676b940677cca6aead219cc79c
Loïse Fenoll <admin@fingertips.fr>
2024-12-05 14:46:12 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/HRnet/', // Remplacez 'repository-name' par le nom de votre dépôt GitHub
-});
+  plugins: [react()]
+})
